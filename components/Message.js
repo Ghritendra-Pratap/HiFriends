873a687081931e012b95473629c8ad4c25b9ad42
Ghritendra Pratap <ghri1810@gmail.com>
2021-08-17 11:25:22 +0530

    initial

---
+++ components/Message.js
@@ -0,0 +1,54 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import styled from 'styled-components';
+import { auth } from '../firebase';
+import moment from "moment";
+
+function Message({user, message}) {
+    const [userloggedIn] =useAuthState(auth)
+
+    const TypeOfMessage = user ===userloggedIn.email? Sender : Reciever;
+    return (
+        <Container>
+            <TypeOfMessage>
+                {message.messages}
+            <Timestamp>
+            {message.timestamp? moment(message.timestamp).format('LT') : '...'}
+            </Timestamp> 
+            </TypeOfMessage>
+            
+        </Container>
+    )
+}
+
+export default Message
+
+const Container = styled.div``;
+
+const MessageElement = styled.p`
+width: fit-content;
+padding: 15px;
+border-radius: 8px;
+margin:10px;
+min-width: 60px;
+padding-bottom: 26px;
+position: relative;
+text-align: right;
+`;
+
+const Timestamp = styled.p`
+font-size: 10px;
+color: grey;
+
+`;
+
+const Sender = styled(MessageElement)`
+background-color: #dcf8c6;
+margin-left: auto;
+
+`;
+
+const Reciever = styled(MessageElement)`
+background-color: whitesmoke;
+text-align: left;
+
+`;
---
+++ pages/Chat.js
@@ -0,0 +1,11 @@
+import Sidebar from "../components/Sidebar";
+
+function chat() {
+    return (
+        <div>
+            <Sidebar/>
+        </div>
+    )
+}
+
+export default chat
