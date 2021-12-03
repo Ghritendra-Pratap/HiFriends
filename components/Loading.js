d277fea3a381f7e3b854117ef9aba57e06080530
Ghritendra Pratap <ghri1810@gmail.com>
2021-12-03 15:59:00 +0530

    previous

--- components/Loading.js
+++ components/Loading.js
@@ -5,10 +5,10 @@ const Loading = () => {
     return (
         <center style = {{ display : "grid" , placeItems : "center" , height : "100vh"}}>
             <div>
-                <Image src="/assets/whatsapp.png"
+                <img src="/assets/logo.jpeg"
                 alt=""
                 style = {{ marginBottom:10}}
-                height ={200} />
+                height ={200}  width={200} layout="fill"/>
                 <Circle color="#3CBC28" size={60}/>
             </div>      
         </center>
